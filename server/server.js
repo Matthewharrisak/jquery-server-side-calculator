65556c6ada6d8e873c5a877407260d7c76121ff0
Matthew Harris <matthewharrisak@gmail.com>
2020-10-10 13:54:51 -0500

    hooked up /mathCalc which stores out math problem objects.

--- server/server.js
+++ server/server.js
@@ -4,8 +4,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
 
+const mathProblems = require ('./modules/mathObjects');
+
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/mathCalc', (req, res) =>{
+    res.send(mathProblems);
+    console.log('hello from /mathCalc');
+    
+})
 
 app.listen(port, () => {
     console.log("Up and running on port: ", port);
