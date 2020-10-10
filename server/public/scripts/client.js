4628de66551b9a4bab8dd583c7d9060c05b746be
Matthew Harris <matthewharrisak@gmail.com>
2020-10-10 13:27:31 -0500

    added -whatsThisEqual() to get values from DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,29 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('hello from JQ');
-    
-    
+    $('#equal').on('click' , whatsThisEqual);
+}
+
+
+//takes in values from DOM and sends them to server /mathCalc to find the sum of the values.
+function whatsThisEqual() {
+    console.log('hello from the equal sign');
+    let mathValues = {
+        numOne: $('#numOne').val, 
+        numTwo: $('#numTwo').val,
+        }
+    console.log(numOne , numTwo);
+    $.ajax({ // POST request allows us to post values to the server where the logic is stored.
+        method: 'POST',
+        url: '/mathCalc',
+        // data: mathProblem
+    }).then(function(response) {
+       console.log('response',  response);
+        $('#numOne').val(''), 
+        $('#numTwo').val('');
+        // get math answer   
+        
+    }).catch(function(error){
+        alert(error);
+    });
 }
